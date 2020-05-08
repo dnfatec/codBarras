601d45f603457a31ff2d694dcb8a93d422bac8e1
dnfatec <39006449+dnfatec@users.noreply.github.com>
2020-05-08 19:45:12 -0300

    Será que vai
    opa !!

--- app/src/main/java/com/example/excodbarn/MainActivity.java
+++ app/src/main/java/com/example/excodbarn/MainActivity.java
@@ -26,8 +26,8 @@ TextView txCodigo;
             public void onClick(View v) {
                 Intent intent = new Intent("com.google.zxing.client.android.SCAN");
                 startActivityForResult(intent,0);
-                Toast.makeText(getApplicationContext(), "teste", Toast.LENGTH_LONG).show();
-                Toast.makeText(getApplicationContext(), "teste", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Alterado no git", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Alterado no git", Toast.LENGTH_LONG).show();
             }
         });
     }
